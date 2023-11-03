fb74551c491586def92c7155a50c12984fbe1bf9
Oghenero <fsone24@gmail.com>
2023-11-03 01:57:02 +0100

    binary trees repair..

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,36 +1,28 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * binary_tree_insert_left -  inserts a node as the left-child of another node
+ * binary_tree_insert_right -  inputs a node as the
+ * right-child of another node
  *
- * @parent: is a pointer to the node to insert the left-child in
- * @value:  is the value to store in the new node
- *
- * Description - If parent already has a left-child, the new node must
- *				take its place, and the old left-child must be set as
- *				the left-child of the new node.
- *
- * Return: a pointer to the created node, or NULL on failure or if
- *			parent is NULL
+ * @parent: Parent of the node
+ * @value: Value of node
+ * Return: A pointer to a newly created node or NULL during failure
  */
-
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *nnode;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
+	nnode = binary_tree_node(NULL, value);
+	if (nnode == NULL)
 		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
-	new->right = NULL;
-	new->left = parent->left;
-	parent->left = new;
-	if (new->left)
-		new->left->parent = new;
-	return (new);
+	nnode->right = parent->right;
+	if (nnode->right != NULL)
+		nnode->right->parent = nnode;
+	parent->right = nnode;
+	nnode->parent = parent;
+	return (nnode);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,22 +1,16 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: tree to measure the height of
- *
- * Return: height of the tree
- *         0 if tree is NULL
+ * binary_tree_postorder - goes through a binary tree using post-order traverse
+ * @tree: tree to traverse
+ * @func: pointer to a function to call for each node
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	if (!tree || !func)
+		return;
 
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
